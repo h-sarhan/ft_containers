d0817ad9bbde5e6d712f0ab2e048b3277f3b069f
Hassan Sarhan <hassanAsarhan@outlook.com>
2023-03-03 00:07:34 +0400

    Declared all member functions of ft::map

--- containers/map.hpp
+++ containers/map.hpp
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 19:06:35 by hsarhan           #+#    #+#             */
-/*   Updated: 2023/03/02 19:16:06 by hsarhan          ###   ########.fr       */
+/*   Updated: 2023/03/03 00:05:23 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,102 @@
 # define MAP_HPP
 
 # include <memory>
-// namespace ft
-// {
+# include "pair.hpp"
+# include "reverse_iterator.hpp"
 
-// 	template < class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<pair<const Key,T> > >
-// 	class map
-// 	{
-		
-// 	};
-// };
+namespace ft
+{
+	template <class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator<pair<const Key, T> > >
+	class map
+	{
+	public:
+		// ** Member types
+		typedef Key											key_type;
+		typedef T											mapped_type;
+		typedef pair<key_type, mapped_type>					value_type;
+		typedef Compare										key_compare;
+		typedef Alloc										allocator_type;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+		// typedef map_iterator<value_type>					iterator;
+		// typedef map_iterator<const value_type>				const_iterator;
+		// typedef ft::reverse_iterator<iterator>				reverse_iterator;
+		// typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
+		// typedef iterator_traits<iterator>::difference_type	difference_type;
+		typedef size_t										size_type;
+
+		// ** Nested value_compare class
+	public:
+		class value_compare : public std::binary_function<value_type, value_type, bool>
+		{
+		protected:
+			key_compare								comp;
+			value_compare(key_compare c) : comp(c){}
+		public:
+			bool operator() (const value_type &x, const value_type &y) const
+			{return comp(x.first, y.first);}
+		};
+
+	public:
+		// ** Constructors and destructors
+		// explicit map(const key_compare &comp = key_compare(), const allocator_type &alloc = allocator_type());
+		// template <class InputIterator>
+		// map(InputIterator first, InputIterator last, const key_compare &comp = key_compare(), const allocator_type &alloc = allocator_type());
+		// map(const map &x);
+		// ~map(void);
+		// map												&operator=(const map &x);
+
+		// ** Iterator functions
+		// iterator										begin(void);
+		// const_iterator 									begin(void) const;
+		// iterator										end(void);
+		// const_iterator									end(void) const;
+		// reverse_iterator								rbegin(void);
+		// const_reverse_iterator							rbegin(void) const;
+		// reverse_iterator								end(void);
+		// const_reverse_iterator							rend(void) const;
+
+		// ** Capacity
+		// bool											empty(void) const;
+		// size_type										size(void) const;
+		// size_type										max_size(void) const;
+
+		// ** Element access
+		// mapped_type											&operator[](const key_type &k);
+		// mapped_type											&at(const key_type& k);
+		// const mapped_type									&at(const key_type &k) const;
+
+		// ** Modifiers
+		// pair<iterator, bool>									insert(const value_type& val);
+		// iterator												insert(iterator position, const value_type &val);
+		// template <class InputIterator>
+		// void													insert(InputIterator first, InputIterator last);
+		// void													erase(iterator position);
+		// size_type											erase(const key_type& k);
+		// void													erase(iterator first, iterator last);
+		// void													swap(map& x);
+		// void													clear(void);
+
+		// ** Observers
+		// key_compare												key_comp(void) const;
+		// value_compare											value_comp(void) const;
+
+		// ** Operations
+		// iterator													find(const key_type &k);
+		// const_iterator												find(const key_type &k) const;
+		// size_type														count(const key_type &k) const;
+		// iterator													lower_bound(const key_type &k);
+		// const_iterator												lower_bound(const key_type &k) const;
+		// iterator													upper_bound(const key_type &k);
+		// const_iterator												upper_bound(const key_type &k) const;
+		// pair<iterator, iterator>											equal_range(const key_type &k);
+		// pair<const_iterator, const_iterator>								equal_range(const key_type &k) const;
+
+		// ** Allocators
+		// allocator_type get_allocator() const;
+	};
+};
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 09:43:47 by hsarhan           #+#    #+#             */
-/*   Updated: 2023/03/02 17:16:59 by hsarhan          ###   ########.fr       */
+/*   Updated: 2023/03/03 00:06:33 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -686,156 +686,6 @@ void	eraseTests(void)
 	b.erase(b.begin(), b.begin() + 1);
 	printVec(b, "");
 }
-#define TESTED_TYPE int
-# include "./containers_test/srcs/base.hpp"
-
-// #define TESTED_NAMESPACE std
-
-#define T_SIZE_TYPE typename TESTED_NAMESPACE::vector<T>::size_type
-
-template <typename T>
-void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = true)
-{
-	const T_SIZE_TYPE size = vct.size();
-	const T_SIZE_TYPE capacity = vct.capacity();
-	const std::string isCapacityOk = (capacity >= size) ? "OK" : "KO";
-	// Cannot limit capacity's max value because it's implementation dependent
-
-	std::cout << "size: " << size << std::endl;
-	std::cout << "capacity: " << isCapacityOk << std::endl;
-	std::cout << "max_size: " << vct.max_size() << std::endl;
-	if (print_content)
-	{
-		typename TESTED_NAMESPACE::vector<T>::const_iterator it = vct.begin();
-		typename TESTED_NAMESPACE::vector<T>::const_iterator ite = vct.end();
-		std::cout << std::endl << "Content is:" << std::endl;
-		for (; it != ite; ++it)
-			std::cout << "- " << *it << std::endl;
-	}
-	std::cout << "###############################################" << std::endl;
-}
-
-int		riteMli(void)
-{
-	const int size = 5;
-	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it_ = vct.begin();
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it(it_);
-
-	std::cout << "dad  " << *(it_) << std::endl;
-	std::cout << "mom  " << *(it) << std::endl;
-	for (int i = 0; i < size; ++i)
-		vct[i] = (i + 1) * 5;
-
-	printSize(vct);
-
-	std::cout << (it_ == it.base()) << std::endl;
-	std::cout << (it_ == (it + 3).base()) << std::endl;
-
-	std::cout << *(it.base() + 1) << std::endl;
-	std::cout << *(it - 3) << std::endl;
-	std::cout << *(it - 3).base() << std::endl;
-	it -= 3;
-	std::cout << *it.base() << std::endl;
-
-	std::cout << "TEST OFFSET" << std::endl;
-	std::cout << *(it) << std::endl;
-	std::cout << *(it).base() << std::endl;
-	std::cout << *(it - 0) << std::endl;
-	std::cout << *(it - 0).base() << std::endl;
-	std::cout << *(it - 1).base() << std::endl;
-
-	return (0);
-}
-
-int		rite2Mli(void)
-{
-	const int size = 5;
-	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it = vct.rbegin();
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_reverse_iterator ite = vct.rbegin();
-	vct[0] = 1;
-	vct[1] = 2;
-	vct[2] = 3;
-	vct[3] = 4;
-	vct[4] = 5;
-	std::cout << *it << std::endl;
-	for (int i = 0; i < size; ++i)
-		it[i] = (size - i) * 5;
-
-	it = it + 5;
-	it = 1 + it;
-	it = it - 4;
-	std::cout << *(it += 2) << std::endl;
-	std::cout << *(it -= 1) << std::endl;
-
-	*(it -= 2) = 42;
-	*(it += 2) = 21;
-
-	std::cout << "const_ite +=/-=: " << *(ite += 2) << " | " << *(ite -= 2) << std::endl;
-
-	std::cout << "(it == const_it): " << (ite == it) << std::endl;
-	std::cout << "(const_ite - it): " << (ite - it) << std::endl;
-	std::cout << "(ite + 3 == it): " << (ite + 3 == it) << std::endl;
-
-	printSize(vct, true);
-	return (0);
-}
-
-#undef TESTED_TYPE
-#define TESTED_TYPE foo<int>
-#undef TESTED_NAMESPACE
-#define TESTED_NAMESPACE ft
-
-int		riteArrow(void)
-{
-	const int size = 5;
-	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
-	vct[0] = 1;
-	vct[1] = 2;
-	vct[2] = 3;
-	vct[3] = 4;
-	vct[4] = 5;
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::reverse_iterator it(vct.rbegin());
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_reverse_iterator ite(vct.rend());
-	
-	// std::cout << *it << "   " << *ite << std::endl;
-
-	for (int i = 1; it != ite; ++i)
-		*it++ = (i * 7);
-	printSize(vct, 1);
-
-	it = vct.rbegin();
-	ite = vct.rbegin();
-
-	std::cout << *(++ite) << std::endl;
-	std::cout << *(ite++) << std::endl;
-	std::cout << *ite++ << std::endl;
-	std::cout << *++ite << std::endl;
-
-	it->m();
-	ite->m();
-
-	std::cout << *(++it) << std::endl;
-	std::cout << *(it++) << std::endl;
-	std::cout << *it++ << std::endl;
-	std::cout << *++it << std::endl;
-
-	std::cout << *(--ite) << std::endl;
-	std::cout << *(ite--) << std::endl;
-	std::cout << *--ite << std::endl;
-	std::cout << *ite-- << std::endl;
-
-	(*it).m();
-	(*ite).m();
-
-	std::cout << *(--it) << std::endl;
-	std::cout << *(it--) << std::endl;
-	std::cout << *it-- << std::endl;
-	std::cout << *--it << std::endl;
-
-	return (0);
-}
 
 void	reverseIteratorTests(void)
 {
@@ -861,67 +711,6 @@ void	reverseIteratorTests(void)
 	// std::cout << *b.rbegin() << std::endl;
 	std::cout << *b.rend() << std::endl;
 }
-
-template <typename Ite_1, typename Ite_2>
-void ft_eq_ope(const Ite_1 &first, const Ite_2 &second, const bool redo = 1)
-{
-	std::cout << (first < second) << std::endl;
-	std::cout << (first <= second) << std::endl;
-	std::cout << (first > second) << std::endl;
-	std::cout << (first >= second) << std::endl;
-	if (redo)
-		ft_eq_ope(second, first, 0);
-}
-
-#undef TESTED_NAMESPACE
-#define TESTED_NAMESPACE ft
-
-int		riteEqOp(void)
-{
-	const int size = 5;
-	TESTED_NAMESPACE::vector<int> vct(size);
-	TESTED_NAMESPACE::vector<int>::reverse_iterator it_0(vct.rbegin());
-	TESTED_NAMESPACE::vector<int>::reverse_iterator it_1(vct.rend());
-	TESTED_NAMESPACE::vector<int>::reverse_iterator it_mid;
-
-	TESTED_NAMESPACE::vector<int>::const_reverse_iterator cit_0 = vct.rbegin();
-	TESTED_NAMESPACE::vector<int>::const_reverse_iterator cit_1;
-	TESTED_NAMESPACE::vector<int>::const_reverse_iterator cit_mid;
-
-	for (int i = size; it_0 != it_1; --i)
-		*it_0++ = i;
-	// printSize(vct, 1);
-	it_0 = vct.rbegin();
-	cit_1 = vct.rend();
-	it_mid = it_0 + 3;
-	cit_mid = it_0 + 3; cit_mid = cit_0 + 3; cit_mid = it_mid;
-
-	std::cout << std::boolalpha;
-	std::cout << ((it_0 + 3 == cit_0 + 3) && (cit_0 + 3 == it_mid)) << std::endl;
-
-	std::cout << "\t\tft_eq_ope:" << std::endl;
-	// regular it
-	ft_eq_ope(it_0 + 3, it_mid);
-	ft_eq_ope(it_0, it_1);
-	
-	std::cout << "it_1 - 3 == " <<  *(it_1 - 3) << std::endl;
-	ft_eq_ope(it_1 - 3, it_mid);
-	// const it
-	ft_eq_ope(cit_0 + 3, cit_mid);
-	ft_eq_ope(cit_0, cit_1);
-	ft_eq_ope(cit_1 - 3, cit_mid);
-	// both it
-	ft_eq_ope(it_0 + 3, cit_mid);
-	ft_eq_ope(it_mid, cit_0 + 3);
-	ft_eq_ope(it_0, cit_1);
-	ft_eq_ope(it_1, cit_0);
-	ft_eq_ope(it_1 - 3, cit_mid);
-	ft_eq_ope(it_mid, cit_1 - 3);
-
-	return (0);
-}
-
-
 
 int	main(void)
 {
@@ -952,17 +741,8 @@ int	main(void)
 	// fillConstructor();
 	// rangeConstructor();
 	// defaultConstructor();
-	// basicTests();
-	// exceptionTests();
-	// basicTests2();
-	// exceptionTests2();
-	// gerardSwap();
 
-	// riteMli();
-	// rite2Mli();
-	// riteArrow();
 	// reverseIteratorTests();
-	riteEqOp();
 	std::cout << std::endl;
 }
 
